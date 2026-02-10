83b6e1a07273af2abc3945ca821a0c317cac62c5
King-Forge <jak1456@gmail.com>
2026-02-10 10:55:37 -0500

    added a footer function component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>&copy; Pretend Event Planner Corporaton. All rights not reserved.</p>
+            </footer>
+        </>
+    );
+};
+
+export default Footer
